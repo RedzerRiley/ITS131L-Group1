12f90992ec065de8625f66914903b0e35ef5ce2f
Redzer Monsod <168085654+RedzerRiley@users.noreply.github.com>
2025-03-10 14:59:37 +0800

    Update TERMINATION-PROCEDURE.sql

--- TERMINATION-PROCEDURE.sql
+++ TERMINATION-PROCEDURE.sql
@@ -1,21 +1,40 @@
-CREATE PROCEDURE EMPLOYEE-TERMINATION (  
-    p_emp_id IN VARCHAR) AS  
-BEGIN  
+CREATE OR REPLACE PROCEDURE terminate_employee (
+    p_emp_id IN VARCHAR2,
+    p_termination_date IN DATE DEFAULT SYSDATE,
+    p_status_after_termination IN VARCHAR2 DEFAULT 'INACTIVE'
+) AS
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_count FROM EMPLOYEE WHERE EMP_ID = p_emp_id;
     
-    DELETE FROM Dependents WHERE EMP_ID = p_emp_id;  
-    DELETE FROM BillingUnit WHERE EMP_ID = p_emp_id;  
-    DELETE FROM CollectionUnit WHERE EMP_ID = p_emp_id;  
-    DELETE FROM PaymentsUnit WHERE EMP_ID = p_emp_id;  
-  
-  
-    DELETE FROM Employee WHERE EMP_ID = p_emp_id;  
-  
-  
-    COMMIT;  
-      
+    IF v_count = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Employee with ID ' || p_emp_id || ' does not exist.');
+    END IF;
     
-    DBMS_OUTPUT.PUT_LINE('Employee with ID ' || p_emp_id || ' has been terminated and all related data deleted.');  
-END;  
-
+    SAVEPOINT before_termination;
+    
+    UPDATE EMPLOYEE 
+    SET EMP_STATUS = p_status_after_termination
+    WHERE EMP_ID = p_emp_id;
+    
+    UPDATE DEPENDENTS
+    SET DEP_STATUS = 'INACTIVE'
+    WHERE EMP_ID = p_emp_id;
+    
+    UPDATE BILLING
+    SET BILLING_STATUS = 'INACTIVE'
+    WHERE EMP_ID = p_emp_id;
+    
+    UPDATE COLLECTION
+    SET COLLECTION_STATUS = 'INACTIVE'
+    WHERE EMP_ID = p_emp_id;
+    
+    UPDATE PAYMENTS
+    SET PAYMENT_STATUS = 'TERMINATED'
+    WHERE EMP_ID = p_emp_id;
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Employee ' || p_emp_id || ' has been successfully terminated.');
+END terminate_employee;
 /
-
